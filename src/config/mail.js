e9b36c0e866b666ad39ec5861f68f73db40eeb7e
Abdallah Ayman <93119611+AbdallahMedo@users.noreply.github.com>
2026-06-03 11:21:00 +0300

    Update mail.js

--- src/config/mail.js
+++ src/config/mail.js
@@ -5,7 +5,10 @@ console.log("MAIL_USER:", process.env.MAIL_USER);
 console.log("MAIL_PASS exists:", !!process.env.MAIL_PASS);
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  // service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
