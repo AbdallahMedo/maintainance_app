13602fc13d1287af5c411f12e0b6ed64c5c5bb55
Abdallah Ayman <93119611+AbdallahMedo@users.noreply.github.com>
2026-06-03 11:07:07 +0300

    Update mail.js

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,9 +1,15 @@
-const nodemailer = require("nodemailer");
-
-module.exports = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS
+  }
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("SMTP Error:", error);
+  } else {
+    console.log("SMTP Ready");
   }
 });
